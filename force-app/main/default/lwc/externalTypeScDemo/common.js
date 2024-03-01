8e5238f0e2a3ec5813f43a6abf15f2e0d7eb12ca
Codewith-AyushGupta <119477183+Codewith-AyushGupta@users.noreply.github.com>
2024-03-01 14:10:13 +0530

    Second Commit

--- force-app/main/default/lwc/externalTypeScDemo/common.js
+++ force-app/main/default/lwc/externalTypeScDemo/common.js
@@ -1,35 +1,24 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EntryPoint = void 0;
-class EntryPoint {
+export class EntryPoint {
     constructor() {
         this.dataTable = document.createElement('table');
         this.container = document.createElement('div');
         console.log('call Invoked');
     }
-    callMethod(address) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const response = yield this.getData(address);
-            this.container = document.createElement('div');
-            if (response && response.entries) {
-                console.log('this is the data', response);
-                this.container = this.displayData(response.entries);
-                console.log('tableElement ===>', this.container);
-                return this.container;
-            }
+    makeStucture(response) {
+        console.log('in makeStructure',response);
+        this.container = document.createElement('div');
+        if (response) {
+            console.log('this is the data', response);
+            this.container = this.addData(response);
+            console.log('tableElement ===>', this.container);
             return this.container;
-        });
+        }
+        return this.container;
     }
-    displayData(data) {
+    addData(data) {
         console.log('this is the data=>', data);
         this.dataTable = document.createElement('table');
         this.container = document.createElement('div');
@@ -58,24 +47,6 @@ class EntryPoint {
         }
         this.container.appendChild(this.dataTable);
         return this.container;
-    }
-    getData(apiUrl) {
-        return __awaiter(this, void 0, void 0, function* () {
-            console.log('in getData');
-            try {
-                const response = yield fetch(apiUrl);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const data = yield response.json();
-                console.log('from getData', data);
-                return data;
-            }
-            catch (error) {
-                console.error('Error fetching data:', error);
-                return null;
-            }
-        });
     }
 }
 exports.EntryPoint = EntryPoint;
--- force-app/main/default/lwc/externalTypeScDemo/externalTypeScDemo.js
+++ force-app/main/default/lwc/externalTypeScDemo/externalTypeScDemo.js
@@ -1,18 +1,56 @@
-import { LightningElement } from 'lwc';
-import {EntryPoint} from './common'
+// ExternalTypeScDemo.js
+import { LightningElement, track } from 'lwc';
+import { EntryPoint } from './common';
 import styles from './externalTypeScDemo.css';
-
+import makeApiCall from '@salesforce/apex/CallExternalAPI.makeApiCall';
+import submiData from '@salesforce/apex/CallExternalAPI.submiData';
 export default class ExternalTypeScDemo extends LightningElement {
-    connectedCallback(){
+    @track show_form = false;
+    @track formFields = {
+        name: '',
+        age: '',
+        colour: ''
+    };
+    handleInputChange(event) {
+        const fieldName = event.target.name;
+        const fieldValue = event.target.value;
+        this.formFields = { ...this.formFields, [fieldName]: fieldValue };
     }
-    async handleButtonClick () {
-        // Add your custom logic here when the button is clicked
+    async handleSubmit() {
+        var data = JSON.stringify(this.formFields);
+        var result
+        await submiData({data:this.formFields})
+            .then((response) => {
+                if(response){
+                    alert('Record Created')
+                }
+            })
+            .catch((error) => {
+                console.error('Error during API call:', error);
+            });
+    }
+    async handleButtonClick() {
         console.log('Button Clicked!');
-         const obj = new EntryPoint();
-        const apiString = 'https://api.publicapis.org/entries';
-        var container = document.createElement('div');
-        container =  await obj.callMethod(apiString);
-        console.log('response',container);
-        this.template.querySelector('.table').appendChild(container);
+        let obj = new EntryPoint();
+        let container = document.createElement('div');
+        var data;
+        await makeApiCall()
+            .then((response) => {
+                console.log(response);
+                data = response;
+            })
+            .catch((error) => {
+                console.error('Error during API call:', error);
+            });
+        if (data) {
+            debugger;
+            container = obj.makeStucture(data);
+            console.log('contaier', container);
+            this.template.querySelector('.table2Master').appendChild(container);
+        }
     }
+    async handlePostCall() {
+        this.show_form = true;
+    }
+
 }
